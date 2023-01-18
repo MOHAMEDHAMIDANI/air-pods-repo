95bfa92a95a022564482a5c6861ac1898fd8f75f
mohamed hamidani <mohamidani1@gmail.com>
2023-01-18 08:41:45 +0100

    just added some files LOL*-*

---
+++ air pods/js/index.js
@@ -0,0 +1,14 @@
+img1 = document.getElementById("img-1");
+img2 = document.getElementById("img-2");
+img3 = document.getElementById("img-3");
+minima = document.getElementById("photo");
+img1.addEventListener("click", function () {
+    minima.src = "../images/pngwing.com (1).png"
+});
+img2.addEventListener("click", function () {
+    minima.src = "../images/pngwing.com (2).png"
+    
+});
+img3.addEventListener("click", function () {
+    minima.src = "../images/pngwing.com.png"
+});
